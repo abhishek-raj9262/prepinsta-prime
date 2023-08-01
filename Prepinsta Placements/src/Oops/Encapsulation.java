22999533a4ce020b4a03c9d4ba4bd46463b38e20
Abhishek Raj <abhishekr359@gmail.com>
2023-08-01 18:48:02 +0530

    encapsulation in oops

---
+++ Prepinsta Placements/src/Oops/Encapsulation.java
@@ -0,0 +1,19 @@
+package Oops;
+public class Encapsulation {
+//    Encapsulation is defined as the wrapping up of data under a single unit. It is the
+//    mechanism that binds together code and the data it manipulates. Another way to
+//    think about encapsulation is, it is a protective shield that prevents the data from
+//    being accessed by the code outside this shield.
+//● Technically in encapsulation, the variables or data of a class is hidden
+//            from any other class and can be accessed only through any member
+//            function of its own class in which they are declared.
+//            ● As in encapsulation, the data in a class is hidden from other classes using
+//            the data hiding concept which is achieved by making the members or
+//            methods of class as private and the class is exposed to the end user or
+//            the world without providing any details behind implementation using the
+//            abstraction concept, so it is also known as combination of data-hiding and
+//    abstraction..
+
+
+    //note - fraction class is the best example of the encapsulation
+}
