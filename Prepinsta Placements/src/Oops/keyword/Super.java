2a235ea6ec151e8f6dfaabba080b30b1c3131c46
Abhishek Raj <abhishekr359@gmail.com>
2023-08-03 20:22:48 +0530

    super keyword brief

--- Prepinsta Placements/src/Oops/keyword/Super.java
+++ Prepinsta Placements/src/Oops/keyword/Super.java
@@ -1,4 +1,8 @@
 package Oops.keyword;
 
 public class Super {
+    // super keyword use in the car class go and check
+    // basically super keyword is used if we have same functions name in super class and subclass,
+    //then super keyword jump to rescue 😂 us to this situation.
+    // super keyword also help in the time of constructor.
 }
