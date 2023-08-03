5acc6e4d251fd4fe279574bac7a062bf159d34bb
Abhishek Raj <abhishekr359@gmail.com>
2023-08-03 20:34:02 +0530

    protected keyword understand

---
+++ Prepinsta Placements/src/Oops/AccessModifier/Protected.java
@@ -0,0 +1,6 @@
+package Oops.AccessModifier;
+
+public class Protected {
+    //this modifier gives us accessibility in subclass of the parent class,
+    //by default it is just like default modifier, but it gives 1 additional accessibility for subclass
+}
