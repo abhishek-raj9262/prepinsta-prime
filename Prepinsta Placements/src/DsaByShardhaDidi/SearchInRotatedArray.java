3ca92a8111eee63315ac911c8c0e9cca2594f028
Abhishek Raj <abhishekr359@gmail.com>
2023-08-05 21:48:33 +0530

    updated

--- Prepinsta Placements/src/DsaByShardhaDidi/SearchInRotatedArray.java
+++ Prepinsta Placements/src/DsaByShardhaDidi/SearchInRotatedArray.java
@@ -8,12 +8,12 @@ public class SearchInRotatedArray {
             int mid = start + (end - start)/2;
             if (target==arr[mid]){
                 return mid;
-            }if (target==arr[start]){
-                return start;
-            }if (target==arr[end]){
-                return end;
-
-            }if (arr[start] < arr[mid]){ //it means that left part of array is sorted
+//            }if (target==arr[start]){
+//                return start;
+//            }if (target==arr[end]){
+//                return end;
+            }
+            if (arr[start] < arr[mid]){ //it means that left part of array is sorted
                 if (target >= arr[start] && target < arr[mid]){
                     end = mid-1;
                 }else {
@@ -34,8 +34,8 @@ public class SearchInRotatedArray {
     }
 
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2};
-        int ans = search(arr,6);
+        int[] arr = {1,1,5};
+        int ans = search(arr,1);
         System.out.println(ans);
     }
 }
