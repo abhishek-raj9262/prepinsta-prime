17cf6385147ec7ee464e24dbadd380ba71ef7b0a
Abhishek Raj <abhishekr359@gmail.com>
2023-08-08 12:09:14 +0530

    changes made in exceptionHandiling

--- Prepinsta Placements/src/Oops/ExceptionHandiling/AccessFractionClass.java
+++ Prepinsta Placements/src/Oops/ExceptionHandiling/AccessFractionClass.java
@@ -3,9 +3,9 @@ package Oops.ExceptionHandiling;
 public class AccessFractionClass {
 
     public static void temp() throws ZeroDenominatorException{
-        Fraction f = new Fraction(10,8);
+        Fraction f = new Fraction(10,0);
         try {
-            f.setDenominator(0);
+            f.setDenominator(7);
         }
         catch (ZeroDenominatorException e){
             System.out.println("hey you cant set zero denominator");
