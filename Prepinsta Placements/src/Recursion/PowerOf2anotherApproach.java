9d44452b7fb86bcfc1b0b6c9abe341964c715121
Abhishek Raj <abhishekr359@gmail.com>
2023-08-09 23:36:51 +0530

    power of 2 via recursive

---
+++ Prepinsta Placements/src/Recursion/PowerOf2anotherApproach.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class PowerOf2anotherApproach {
+    public static boolean isPowerOfTwo(int n) {
+        if (n==0){
+            return false;
+        }if (n==1){
+            return true;
+        }if (n%2!=0){
+            return false;
+
+        }
+        return isPowerOfTwo(n/2);
+
+    }
+
+    public static void main(String[] args) {
+        boolean ans = isPowerOfTwo(17);
+        System.out.println(ans);
+    }
+}
