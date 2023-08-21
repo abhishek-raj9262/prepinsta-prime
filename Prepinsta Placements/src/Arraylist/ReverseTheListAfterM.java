e0b01e0aaf143c5b97dc4d03f0a0e16b827b019f
Abhishek Raj <abhishekr359@gmail.com>
2023-08-21 19:12:35 +0530

    revrese the list after m - add

--- Prepinsta Placements/src/Arraylist/ReverseTheListAfterM.java
+++ Prepinsta Placements/src/Arraylist/ReverseTheListAfterM.java
@@ -3,16 +3,14 @@ import java.util.ArrayList;
 public class ReverseTheListAfterM {
     public static void reverseArray(int[]  arr, int m)
     {
-        int count = 0;
+        int count = m;
         int n = arr.length-1;
-        for (int i = 0; i < arr.length; i++) {
-            count++;
-            if (count == m){
+        for (int i = count; i < n; i++) {
                 int temp = arr[count+1];
                 arr[count+1] = arr[n];
                 arr[n] = temp;
                 n--;
-            }
+                count++;
 
         }
 
@@ -25,8 +23,8 @@ public class ReverseTheListAfterM {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        reverseArray(arr,2);
+        int[] arr = {1,2,3,4,5,6,7};
+        reverseArray(arr,1);
         print(arr);
 
     }
