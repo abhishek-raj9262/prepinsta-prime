2e23f31eaf727431bd7858cdd8ebd740dfe9bc25
Abhishek Raj <abhishekr359@gmail.com>
2023-09-14 00:40:11 +0530

    two sum practice

--- Prepinsta Placements/src/Practice/p.java
+++ Prepinsta Placements/src/Practice/p.java
@@ -1,8 +1,29 @@
 package Practice;
-
 public class p {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        for(int i = 0; i<nums.length-1;i++){
+            for(int j = i+1; j<nums.length;j++){
+                if(nums[i] + nums[j] == target){
+                    arr[0] = i;
+                    arr[1] = j;
+                }
+            }
+        }
+        return arr;
+
+
+    }
+    public static void print(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+
+        }
+    }
     public static void main(String[] args) {
-        int a = 67;
-        System.out.println(a);
+        int[] arr = {3,2,3};
+        int[] ans = twoSum(arr,6);
+        print(ans);
+
     }
 }
