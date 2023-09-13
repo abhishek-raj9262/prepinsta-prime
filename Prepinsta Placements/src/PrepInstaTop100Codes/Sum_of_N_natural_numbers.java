937bdbe864ec10593b4de11ee3ea981b87545cfe
Abhishek Raj <abhishekr359@gmail.com>
2023-09-14 01:00:18 +0530

    top100 by prepinsta attemp question 4

---
+++ Prepinsta Placements/src/PrepInstaTop100Codes/Sum_of_N_natural_numbers.java
@@ -0,0 +1,14 @@
+package PrepInstaTop100Codes;
+
+public class Sum_of_N_natural_numbers {
+    public static void main (String[]args)
+    {
+
+        int n = 10;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++)
+            sum += i;
+        System.out.println (sum);
+    }
+}
